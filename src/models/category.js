05164f94a65c6d215df45b04d79440ab8243411a
Windson Macedo <windsondev@hotmail.com>
2022-12-02 19:03:47 -0300

    feat: criação da model Category

---
+++ src/models/category.js
@@ -0,0 +1,16 @@
+const categoriesModel = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  }
+  );
+  return Category
+};
+
+module.exports = categoriesModel;
