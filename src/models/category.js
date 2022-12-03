493a547b3ec20bf268c7dc49a096e7d975367a74
Windson Macedo <windsondev@hotmail.com>
2022-12-02 23:45:39 -0300

    tentando requisito 7

--- src/models/category.js
+++ src/models/category.js
@@ -1,10 +1,5 @@
 const categoriesModel = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     name: DataTypes.STRING,
   },
   {
